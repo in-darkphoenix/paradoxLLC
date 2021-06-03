ac66bfc35bf2eb40f953aa1f0be7bcc312961faa
Ankit <ph,official.ankit@gmail.com>
2021-06-03 19:01:12 +0530

    add data

---
+++ Server/data/subject.js
@@ -0,0 +1,170 @@
+const t1 = {
+  code: [
+    "ETMA-101",
+    "ETPH-103",
+    "ETME-105",
+    "ETEE-107",
+    "ETHS-109",
+    "ETCS-111",
+    "ETCH-113",
+  ],
+  sub: [
+    "Applied Mathematics-I",
+    "Applied Physics-I",
+    "Manufacturing Processes",
+    "Electrical Technology",
+    "Human Values and Professional Ethics-I",
+    "Fundamentals of Computing",
+    "Applied Chemistry",
+  ],
+  cred: [4, 3, 3, 3, 1, 2, 3],
+};
+
+const p1 = {
+  code: [
+    "ETPH - 151",
+    "ETEE - 153",
+    "ETME - 155",
+    "ETME - 157",
+    "ETCS - 157",
+    "ETCH - 161",
+  ],
+  sub: [
+    "Applied Physics Lab-I",
+    "Electrical Technology Lab",
+    "Engineering Graphics Lab",
+    "Fundamentals of Computing Lab",
+    "Applied Chemistry Lab",
+  ],
+  cred: [1, 1, 2, 1, 2, 1],
+};
+
+const t2 = {
+  code: [
+    "ETMA-102",
+    "ETPH-104",
+    "ETEC-106",
+    "ETCS-108",
+    "ETME-110",
+    "ETHS-112",
+    "ETEN-114",
+  ],
+  sub: [
+    "Applied Mathematics-II",
+    "Applied Physics-II",
+    "Electronic Devices",
+    "Introduction to Programming",
+    "Engineering Mechanics",
+    "Communication Skills",
+    "Environmental Studies",
+  ],
+  cred: [4, 3, 3, 3, 3, 3, 3],
+};
+
+const p2 = {
+  code: ["ETPH-152", "ETCS-154", "ETEC-156", "ETME-158", "ETEN-160"],
+  code: [
+    "Applied Physics Lab-II",
+    "Programming Lab",
+    "Electronic Devices Lab",
+    "Engineering Mechanics Lab",
+    "Environmental Studies Lab",
+  ],
+  cred: [1, 1, 1, 1, 1],
+};
+
+const t3 = {
+  code: [
+    "ETMA 201",
+    "ETCS 203",
+    "ETEC 205",
+    "ETEE 207",
+    "ETCS 209",
+    "ETCS 211",
+  ],
+  sub: [
+    "Applied Mathematics – III",
+    "Foundation of Computer Science",
+    "Switching Theory and Logic Design",
+    "Circuits and Systems",
+    "Data Structure",
+    "Computer Graphics and Multimedia",
+  ],
+  cred: [4, 4, 4, 4, 4, 4],
+};
+
+const p3 = {
+  code: ["ETEC-253", "ETCS-255", "ETEE-257", "ETCS-257"],
+  sub: [
+    "Switching Theory and Logic Design Lab",
+    "Data Structure Lab",
+    "Circuits and Systems Lab",
+    "Computer Graphics and Multimedia Lab",
+  ],
+  cred: [1, 1, 1, 1],
+};
+
+const t4 = {
+  code: [
+    "ETMA 202",
+    "ETCS 204",
+    "ETCS 206",
+    "ETCS 208",
+    "ETCS 210",
+    "ETEC 212",
+  ],
+  sub: [
+    "Applied Mathematics – IV",
+    "Computer Organization and Architecture",
+    "Theory of Computation",
+    "Database Management Systems",
+    "Object Oriented Programming",
+    "Communication Systems",
+  ],
+  cred: [4, 4, 4, 4, 3, 4],
+};
+
+const p4 = {
+  code: ["ETMA 252", "ETCS 260", "ETCS 256", "ETCS 258", "ETEC 256"],
+  sub: [
+    "Applied Mathematics Lab",
+    "Computer Organization and Architecture Lab",
+    "Database Management Systems Lab",
+    "Object Oriented Programming Lab",
+    "Communication Systems Lab",
+  ],
+  cred: [1, 1, 1, 1, 1],
+};
+
+const t5 = {
+  code: ["ETCS301", "ETCS303", "ETCS 307", "ETMS 311", "ETEC-303", "ETHS 301"],
+  sub: [
+    "Algorithms Design and Analysis",
+    "Software Engineering",
+    "Java Programming",
+    "Industrial Management",
+    "Digital Communication",
+    "Communication Skills for Professionals",
+  ],
+  cred: [4, 4, 4, 3, 4, 1],
+};
+
+const p5 = {
+  code: [
+    "ETCS 351",
+    "ETCS 353",
+    "ETCS 357",
+    "ETCS 359",
+    "ETEC-357",
+    "ETHS 351",
+  ],
+  sub: [
+    "Algorithms Design and Analysis Lab",
+    "Software Engineering Lab",
+    "Java Programming Lab",
+    "Viva Industrial Training / In-house Workshop",
+    "Digital Communication Lab",
+    "Communication Skills for Professionals Lab",
+  ],
+  cred: [1, 1, 1, 1, 1, 1],
+};
